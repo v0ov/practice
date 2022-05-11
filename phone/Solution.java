20b4d84a7bb8c778382daf6078327b8205265288
v0ov <v0ov@naver.com>
2022-05-11 23:08:08 +0900

    List Stream

--- phone/Solution.java
+++ phone/Solution.java
@@ -1,9 +1,15 @@
 package phone;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) {
 
-        String[] phone_book = new String[] {"12","123","1235","567","88"};
+        String[] phone_book = new String[] {"12","123","88","1235","567"};
 
+        List list = Arrays.asList(phone_book).stream().sorted().toList();
+
+        System.out.println(list);
     }
 }
