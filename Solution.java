a59ef941a2447e2646d575e6be4256a7ec397673
v0ov <v0ov@naver.com>
2022-05-11 22:12:36 +0900

    List Stream

--- Solution.java
+++ Solution.java
@@ -11,6 +11,12 @@ public class Solution {
         String[] completion = new String[] {"stanko", "ana", "mislav"};
 
         Map<String, Long> pMap = Arrays.asList(participant).stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        //찾아서 한부분
+        // 배열을 리스트로 받는법 asList
+        // 스트림에서 그룹바이 하는 법 collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+        // 그룹바이 한것을 맵으로 받는 법
+        // ?? asList로 배열을 리스트로 받은걸 스트림 그룹바이로 어떻게 맵으로 한번에 받을수있게 되는지 모르겟다
+
 
         for (String complete : completion){
             Long val = pMap.get(complete) - 1;
